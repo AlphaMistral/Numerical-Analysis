83c34965c7752313457bea2cb23a852c7eca4c22
于京平 <597379157@qq.com>
2016-01-28 11:34:08 +0800

    Initial Commit

---
+++ CPPTest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CPPTest
+//
+//  Created by 于京平 on 16/1/28.
+//  Copyright © 2016年 于京平. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
