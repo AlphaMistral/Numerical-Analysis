10c633ed088f60c1185af7094be2c7d2425a6ed2
yujingping <Mistral@foxmail.com>
2016-02-24 14:46:37 +0800

    Well, just initialized the ugly-looking default codes HHHHHH

--- CPPTest/main.cpp
+++ CPPTest/main.cpp
@@ -1,15 +1,11 @@
-//
-//  main.cpp
-//  CPPTest
-//
-//  Created by 于京平 on 16/1/28.
-//  Copyright © 2016年 于京平. All rights reserved.
-//
+#include<iostream>
+#include<cstring>
+#include<cstdio>
+#include<cstdlib>
+#include<algorithm>
+using namespace std;
 
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int main()
+{
+    
 }
