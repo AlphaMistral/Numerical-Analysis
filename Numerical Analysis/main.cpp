43a4af8ba44376cd1651c446086a9aed8e464289
Yujingping <Mistral@weymire.com>
2016-07-22 15:32:33 +0800

    Initial Commit

---
+++ Numerical Analysis/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Numerical Analysis
+//
+//  Created by 于京平 on 16/7/22.
+//  Copyright © 2016年 于京平. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
